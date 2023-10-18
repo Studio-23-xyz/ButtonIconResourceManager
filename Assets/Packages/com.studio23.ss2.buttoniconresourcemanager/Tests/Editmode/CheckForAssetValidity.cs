a103a76e0464542d3b6393e5b5e0e1bb6a587b90
N-Evan <76874563+N-Evan@users.noreply.github.com>
2023-10-18 17:23:50 +0600

    Updated Editmode test cases and bumped version number

--- Assets/Packages/com.studio23.ss2.buttoniconresourcemanager/Tests/Editmode/CheckForAssetValidity.cs
+++ Assets/Packages/com.studio23.ss2.buttoniconresourcemanager/Tests/Editmode/CheckForAssetValidity.cs
@@ -8,13 +8,13 @@ public class CheckForAssetValidity
 	public void _Check_For_19_Sprite_Asset_In_ButtonIcons_PS_Folder_()
 	{
 		var psSprites = Resources.LoadAll<Texture2D>($"ButtonIcons/PS/");
-		Assert.AreEqual(19, psSprites.Length);
+		Assert.AreEqual(20, psSprites.Length);
 	}
 
 	[Test]
 	public void _Check_For_19_KeyIcon_Asset_Exists_In_Directory_()
 	{
 		var scriptableObjects = Resources.LoadAll<KeyIcons>($"KeyIcons");
-		Assert.AreEqual(19, scriptableObjects.Length);
+		Assert.AreEqual(20, scriptableObjects.Length);
 	}
 }
