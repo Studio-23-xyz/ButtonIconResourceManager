684773ea03fcdd1d379ec2b02a7001e21091ecac
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2026-08-10 11:13:52 +0600

    Merge pull request #13 from NahiyanSamit/main
    added/fixed some mapping

---
+++ Editor/ButtonIconSOGenerator.cs
@@ -0,0 +1,52 @@
+using Studio23.SS2.ButtonIconResourceManager.Data;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Studio23.SS2.ButtonIconResourceManager.Editor
+{
+	public class ButtonIconSOGenerator : EditorWindow
+	{
+		[MenuItem("Studio-23/Button Icon Resource Manager/Create ButtonIcon Objects")]
+		public static void CreateButtonSOs()
+		{
+            string keyBoardFolderPath = "ButtonIcons/KeyBoard";
+            string xboxFolderPath = "ButtonIcons/Xbox";
+			string playstationFolderPath = "ButtonIcons/PS";
+
+            Sprite[] keyBoardSprites = Resources.LoadAll<Sprite>($"{keyBoardFolderPath}");
+            Sprite[] xboxSprites = Resources.LoadAll<Sprite>($"{xboxFolderPath}");
+			Sprite[] psSprites = Resources.LoadAll<Sprite>($"{playstationFolderPath}");
+
+			if (Directory.Exists($"Assets/Resources/KeyIcons/"))
+			{
+				Directory.Delete($"Assets/Resources/KeyIcons/", true);
+			}
+
+			Directory.CreateDirectory($"Assets/Resources/KeyIcons/GamePad/");
+			for (var i = 0; i < xboxSprites.Length; i++)
+			{
+				GamepadKeyIcons keyIcons = CreateInstance<GamepadKeyIcons>();
+				var controlPath = xboxSprites[i].name;
+				EditorUtility.SetDirty(keyIcons);
+				keyIcons.Initialize(xboxSprites[i], psSprites[i], controlPath);
+
+				AssetDatabase.CreateAsset(keyIcons, $"Assets/Resources/KeyIcons/GamePad/{controlPath}.asset");
+			}
+
+            Directory.CreateDirectory($"Assets/Resources/KeyIcons/KeyBoard/");
+            for (var i = 0; i < keyBoardSprites.Length; i++)
+            {
+                KeyBoardIcons keyIcons = CreateInstance<KeyBoardIcons>();
+                var controlPath = keyBoardSprites[i].name;
+                EditorUtility.SetDirty(keyIcons);
+                keyIcons.Initialize(keyBoardSprites[i], controlPath);
+
+                AssetDatabase.CreateAsset(keyIcons, $"Assets/Resources/KeyIcons/KeyBoard/{controlPath}.asset");
+            }
+
+            AssetDatabase.Refresh();
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
---
+++ Runtime/Data/KeyIconMapping.cs
@@ -0,0 +1,138 @@
+using System.Collections.Generic;
+
+namespace Studio23.SS2.ButtonIconResourceManager.Data
+{
+    internal class KeyIconMappings
+    {
+        private readonly Dictionary<string, string> _keyCharacterMappings;
+
+        public KeyIconMappings()
+        {
+            _keyCharacterMappings = new Dictionary<string, string>(System.StringComparer.OrdinalIgnoreCase)
+            {
+                // Numbers
+                { "0", "numpad0" },
+                { "1", "numpad1" },
+                { "2", "numpad2" },
+                { "3", "numpad3" },
+                { "4", "numpad4" },
+                { "5", "numpad5" },
+                { "6", "numpad6" },
+                { "7", "numpad7" },
+                { "8", "numpad8" },
+                { "9", "numpad9" },
+
+                { "Num 0", "numpad0" },
+                { "Num 1", "numpad1" },
+                { "Num 2", "numpad2" },
+                { "Num 3", "numpad3" },
+                { "Num 4", "numpad4" },
+                { "Num 5", "numpad5" },
+                { "Num 6", "numpad6" },
+                { "Num 7", "numpad7" },
+                { "Num 8", "numpad8" },
+                { "Num 9", "numpad9" },
+
+                // Modifiers
+                { "Left Shift", "shift" },
+                { "Right Shift", "shift" },
+                { "Shift", "shift" },
+
+                { "Left Ctrl", "ctrl" },
+                { "Right Ctrl", "ctrl" },
+                { "Ctrl", "ctrl" },
+                { "Control", "ctrl" },
+
+                { "Left Alt", "alt" },
+                { "Right Alt", "alt" },
+                { "Alt", "alt" },
+
+                { "Left Windows", "leftMeta" },
+                { "Right Windows", "leftMeta" },
+                { "Windows", "leftMeta" },
+                { "Left Meta", "leftMeta" },
+                { "Right Meta", "leftMeta" },
+
+                // Navigation
+                { "Up Arrow", "upArrow" },
+                { "Down Arrow", "downArrow" },
+                { "Left Arrow", "leftArrow" },
+                { "Right Arrow", "rightArrow" },
+                { "Up", "upArrow" },
+                { "Down", "downArrow" },
+                { "Left", "leftArrow" },
+                { "Right", "rightArrow" }, 
+                
+                { "Page Up", "pageUp" },
+                { "Page Down", "pageDown" },
+
+                // Editing
+                { "Backspace", "backspace" },
+                { "Enter", "enter" },
+                { "Num Enter", "numpadEnter" },
+                { "Escape", "escape" },
+                { "Esc", "escape" },
+                { "Caps Lock", "capsLock" },
+
+                // Numpad Operators
+                { "Num +", "numpadPlus" },
+                { "+", "numpadPlus" },
+                { "Num -", "numpadMinus" },
+                { "Num *", "numpadMultiply" },
+                { "*", "numpadMultiply" },
+                { "Num /", "numpadDivide" },
+                { "Num del", "period" },
+
+                // Symbols
+                { "-", "minus" },
+                { "=", "equals" },
+                { "[", "leftBracket" },
+                { "]", "rightBracket" },
+                { "\\", "backslash" },
+                { ";", "semicolon" },
+                { "'", "quote" },
+                { ",", "comma" },
+                { ".", "period" },
+                { "/", "slash" },
+                { "`", "backquote" },
+
+                // Misc
+                { "Print Screen", "printScreen" },
+                { "Scroll Lock", "scrollLock" },
+                
+                // Mouse
+                {"LMB", "mouse0"},
+                {"RMB", "mouse1"},
+                {"MMB", "mouse2"},
+                {"delta", "mouse"},
+                {"position", "mouse"},
+                {"forward", "mouse"},
+                {"back", "mouse"},
+                
+                // GamePad
+                {"LS/Up","LSUp"},
+                {"LS/Down","LSDown"},
+                {"LS/Left","LSLeft"},
+                {"LS/Right","LSRight"},
+                {"RS/Up","RSUp"},
+                {"RS/Down","RSDown"},
+                {"RS/Left","RSLeft"},
+                {"RS/Right","RSRight"},
+                {"D-Pad/Up","D-PadUp"},
+                {"D-Pad/Down","D-PadDown"},
+                {"D-Pad/Left","D-PadLeft"},
+                {"D-Pad/Right","D-PadRight"},
+                {"Back","Select"},
+            };
+        }
+
+        public string GetSpriteName(string displayName)
+        {
+            if (_keyCharacterMappings.TryGetValue(displayName, out var spriteName))
+                return spriteName;
+
+            // Fallback
+            return displayName;
+        }
+    }
+}
